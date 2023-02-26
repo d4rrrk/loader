7075f6a47f165c9b06a120a9636848d220608392
d4rrrk <58345897+d4rrrk@users.noreply.github.com>
2023-02-26 23:34:56 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 537413528 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/BABFTautoFarm.lua'))()
+elseif game.PlaceId == 10138901829 or game.PlaceId == 10371908957 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/d4rrrk/roblox-scripts/main/fakewokenChestFarm.lua'))()
+end
